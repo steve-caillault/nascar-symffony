5923cfcae25c83d5e30f591bf1583f3028e1a5e4
Stève Caillault <steve.caillault@gmail.com>
2021-07-23 09:05:57 +0200

    Menu de l'utilisateur pour le panneau d'administration.

--- src/Controller/Admin/AdminAbstractController.php
+++ src/Controller/Admin/AdminAbstractController.php
@@ -11,10 +11,29 @@ use App\UI\Menus\Breadcrumb\{
     Breadcrumb,
     BreadcrumbItem
 };
+use App\UI\Admin\Menus\Header\UserMenu;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class AdminAbstractController extends BaseAbstractController implements AdminControllerInterface {
 
-     /**
+    /**
+     * Menu de l'utilisateur
+     * @var UserMenu
+     */
+    private ?UserMenu $user_menu = null;
+
+    /**
+     * Initialise le menu de l'utilisateur
+     * @param UserMenu
+     * @return void
+     * @required
+     */
+    public function setUserMenu(UserMenu $userMenu)
+    {
+       $this->user_menu = $userMenu;
+    }
+
+    /**
      * Initialise le fil d'ariane
      * @param Breadcrumb $breadcrumb
      * @return void
@@ -25,7 +44,29 @@ abstract class AdminAbstractController extends BaseAbstractController implements
         parent::setBreadcrumb($breadcrumb);
 
         $breadcrumb->addItem(new BreadcrumbItem('admin.label', 'admin.alt', 'app_admin_default_index'));
+    }
 
+    /**
+     * Remplis l'en-tête avec les menus
+     * @return void
+     */
+    protected function fillHeaderMenus() : void
+    {
+        parent::fillHeaderMenus();
+        $this->getHeaderMenus()->add($this->user_menu);
+    }
+
+    /**
+     * Renders a view.
+     * @param string $view
+     * @param array $parameters
+     * @param Response
+     * @return Response
+     */
+    protected function render(string $view, array $parameters = [], Response $response = null) : Response
+    {
+        $this->fillHeaderMenus();
+        return parent::render($view, $parameters, $response);
     }
 
 }
---
+++ src/UI/Admin/Menus/Header/UserMenu.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Menu de l'utilisateur connecté au panneau d'administration
+ */
+
+namespace App\UI\Admin\Menus\Header;
+
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Contracts\Translation\TranslatorInterface;
+/***/
+use App\UI\Menus\Header\{
+    HeaderMenu,
+    HeaderItemMenu
+};
+use App\Entity\User;
+
+final class UserMenu extends HeaderMenu {
+
+    /**
+	 * Le type de menu
+	 * @var string $type
+	 */
+	protected string $type = self::TYPE_PRIMARY;
+
+    /**
+     * Utilisateur connecté
+     * @var ?User
+     */
+    private ?User $user;
+
+    /**
+     * Constructeur
+     * @param Security $security
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(Security $security, private TranslatorInterface $translator)
+    {
+        $user = $security->getUser();
+        $this->user = $security->getUser();
+    }
+
+    /**
+	 * Alimente le menu avec les éléments du menu
+	 * @return void
+	 */
+	protected function fill() : void
+    {
+        if($this->user === null)
+        {
+            return;
+        }
+
+        $userNameItem = (new HeaderItemMenu())
+            ->setLabel($this->user->getFullName())
+        ;
+
+        $logoutItem = (new HeaderItemMenu())
+            ->setLabel($this->translator->trans('header.admin.user.logout.label', domain: 'menus'))
+            ->setAltLabel($this->translator->trans('header.admin.user.logout.alt_label', domain: 'menus'))
+            ->setRouteName('app_admin_security_auth_logout');
+
+        $this->addItems([ 
+            $userNameItem, 
+            $logoutItem,
+        ]);
+    }
+
+}
