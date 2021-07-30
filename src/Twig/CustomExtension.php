1e4ceb3fd8d074529a6fd8b5a3a585a24794ff55
Stève Caillault <steve.caillault@gmail.com>
2021-07-30 17:07:26 +0200

    Fonction Twig pour afficher une ancre.

--- src/Twig/CustomExtension.php
+++ src/Twig/CustomExtension.php
@@ -23,8 +23,23 @@ final class CustomExtension extends AbstractExtension {
 
         return [
             new TwigFunction('pagination', [ PaginationRuntime::class, 'getRender', ], $defaultOptions),
+            new TwigFunction('anchor', [ $this, 'anchorRender', ], $defaultOptions),
         ];
     }
 
-
+    /**
+     * Retourne une ancre
+     * @param string $label Texte de l'ancre
+     * @param string $altLabel Texte alternatif
+     * @param string $url
+     * @return string
+     */
+    public function anchorRender(string $label, string $altLabel, string $url) : string
+    {
+        return strtr('<a href=":url" title=":alt">:label</a>', [
+            ':label' => $label,
+            ':alt' => $altLabel,
+            ':url' => $url,
+        ]);
+    }
 }
