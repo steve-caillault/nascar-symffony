e8d0a77eb0c24181969d48ef9c11e90de98806de
Stève Caillault <steve.caillault@gmail.com>
2021-08-05 15:48:43 +0200

    Ajout de l'index unique sur l'année de la saison.

--- migrations/Version20210730135815.php
+++ migrations/Version20210730135815.php
@@ -20,7 +20,7 @@ final class Version20210730135815 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE seasons (id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL, year SMALLINT UNSIGNED NOT NULL, state VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE seasons (id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL, year SMALLINT UNSIGNED NOT NULL, state VARCHAR(10) NOT NULL, PRIMARY KEY(id), UNIQUE INDEX idx_year (year)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
