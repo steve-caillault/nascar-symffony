edd2098ee2388f3b49082930c59de9f64b0d26c5
Stève Caillault <steve.caillault@gmail.com>
2021-08-24 16:11:59 +0200

    Menu des états.

--- src/UI/Admin/Menus/Header/CountriesMenu.php
+++ src/UI/Admin/Menus/Header/CountriesMenu.php
@@ -11,16 +11,25 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 /***/
 use App\UI\Menus\Header\HeaderMenu;
-use App\Entity\Country;
+use App\Entity\{
+    Country,
+    CountryState
+};
 use App\UI\Menus\Header\HeaderItemMenu;
 
 final class CountriesMenu extends HeaderMenu {
 
     /**
-     * Country géré
+     * Pays géré
      * @var ?Country
      */
     private ?Country $country = null;
+
+    /**
+     * Etat géré
+     * @var ?CountryState
+     */
+    private ?CountryState $countryState = null;
 
     /**
      * Constructeur
@@ -59,6 +68,17 @@ final class CountriesMenu extends HeaderMenu {
     }
 
     /**
+     * Modifie l'état à gérer
+     * @param CountryState
+     * @return self
+     */
+    public function setCountryState(CountryState $countryState) : self
+    {
+        $this->countryState = $countryState;
+        return $this;
+    }
+
+    /**
      * Identifiant du menu
      * @return string
      */
@@ -81,7 +101,7 @@ final class CountriesMenu extends HeaderMenu {
 
         if($this->country !== null)
         {
-            $this->addCountryItem();
+            $this->addCountryItems();
         }
         
     }
@@ -132,14 +152,32 @@ final class CountriesMenu extends HeaderMenu {
      * Ajoute les éléments de gestion d'un pays
      * @return self
      */
-    private function addCountryItem() : self
+    private function addCountryItems() : self
     {
         $this->addEditItem();
+        $this->addStatesListItem();
+        $this->addStateCreationItem();
+
+        if($this->countryState !== null)
+        {
+            $this->addCountryStateItems();
+        }
+
         return $this;
     }
 
     /**
-     * Ajout l'élément d'édition d'un pays
+     * Ajoute les éléments de gestion d'un état
+     * @return self
+     */
+    private function addCountryStateItems() : self
+    {
+        $this->addEditStateItem();
+        return $this;
+    }
+
+    /**
+     * Ajoute l'élément d'édition d'un pays
      * @return self
      */
     private function addEditItem() : self
@@ -154,9 +192,91 @@ final class CountriesMenu extends HeaderMenu {
             ], domain: 'menus'))
             ->setRouteName('app_admin_countries_edit_index')
             ->setRouteParameters([
-                'countryCode' => $country->getCode(),
+                'countryCode' => strtolower($country->getCode()),
             ])
         ;
+
+        if($currentRouteName === $item->getRouteName())
+        {
+            $item->addClass('selected');
+        }
+
+        return $this->addItem($item);
+    }
+
+    /**
+     * Ajoute l'élément d'édition de l'état
+     * @return self
+     */
+    private function addEditStateItem() : self
+    {
+        $currentRouteName = $this->getCurrentRouteName();
+        $countryState = $this->countryState;
+
+        $item = (new HeaderItemMenu())
+            ->setLabel($this->translator->trans('header.admin.countries.states.edit.label', domain: 'menus'))
+            ->setAltLabel($this->translator->trans('header.admin.countries.states.edit.alt_label', [
+                'name' => $countryState->getName(),
+            ], domain: 'menus'))
+            ->setRouteName('app_admin_countries_states_edit_index')
+            ->setRouteParameters([
+                'countryStateCode' => $countryState->getCode(),
+                'countryCode' => strtolower($countryState->getCountry()->getCode()),
+            ])
+        ;
+
+        if($currentRouteName === $item->getRouteName())
+        {
+            $item->addClass('selected');
+        }
+
+        return $this->addItem($item);
+    }
+
+    /**
+     * Ajoute l'élément de la liste des états du pays
+     * @return self
+     */
+    private function addStatesListItem() : self
+    {
+        $currentRouteName = $this->getCurrentRouteName();
+
+        $item = (new HeaderItemMenu())
+            ->setLabel($this->translator->trans('header.admin.countries.states.list.label', domain: 'menus'))
+            ->setAltLabel($this->translator->trans('header.admin.countries.states.list.alt_label', [
+                'name' => $this->country->getName(),
+            ], domain: 'menus'))
+            ->setRouteName('app_admin_countries_states_list_index')
+            ->setRouteParameters([
+                'countryCode' => strtolower($this->country->getCode()),
+            ])
+        ;
+
+        if($currentRouteName === $item->getRouteName())
+        {
+            $item->addClass('selected');
+        }
+
+        return $this->addItem($item);
+    }
+
+    /**
+     * Ajout l'élément de l'ajout d'un état à un pays
+     * @return self
+     */
+    private function addStateCreationItem() : self
+    {
+        $currentRouteName = $this->getCurrentRouteName();
+
+        $item = (new HeaderItemMenu())
+            ->setLabel($this->translator->trans('header.admin.countries.states.add.label', domain: 'menus'))
+            ->setAltLabel($this->translator->trans('header.admin.countries.states.add.alt_label', [
+                'name' => $this->country->getName(),
+            ], domain: 'menus'))
+            ->setRouteName('app_admin_countries_states_add_index')
+            ->setRouteParameters([
+                'countryCode' => strtolower($this->country->getCode()),
+            ]);
 
         if($currentRouteName === $item->getRouteName())
         {
