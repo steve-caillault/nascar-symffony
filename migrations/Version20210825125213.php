00058b0d62c5df951d9135f5a6db0de3e3ac2ef2
Stève Caillault <steve.caillault@gmail.com>
2021-08-25 14:59:00 +0200

    Migration des villes.

---
+++ migrations/Version20210825125213.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210825125213 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE cities (id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL, state VARCHAR(3) NOT NULL, name VARCHAR(50) NOT NULL, latitude FLOAT NOT NULL, longitude FLOAT NOT NULL, INDEX fk_state (state), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE cities ADD CONSTRAINT FK_D95DB16BA393D2FB FOREIGN KEY (state) REFERENCES countries_states (code) ON DELETE CASCADE ON UPDATE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE cities');
+    }
+}
