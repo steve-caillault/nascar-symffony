49893d9f8102a2648fb51be1204727eec17add37
Stève Caillault <steve.caillault@gmail.com>
2021-11-27 18:18:13 +0100

    Ajout de l'entité CarModel.

---
+++ src/Entity/CarModel.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints;
+/***/
+use App\Repository\CarModelRepository;
+
+#[
+    ORM\Table('cars_models'),
+    ORM\Entity(repositoryClass: CarModelRepository::class),
+    ORM\Index(fields: [ 'motor' ], name: 'idx_motor'),
+    UniqueEntity(fields: [ 'motor', 'name'], message: 'car_models.edit.name.not_exists')
+]
+class CarModel
+{
+    /**
+     * Identifiant en base de données
+     * @var ?int 
+     */
+    #[
+        ORM\Id,
+        ORM\GeneratedValue,
+        ORM\Column(type: 'smallint', options: [ 'unsigned' => true ])
+    ]
+    private ?int $id = null;
+
+    /**
+     * Moteur
+     * @var ?Motor
+     */
+    #[
+        ORM\ManyToOne(targetEntity: Motor::class),
+        ORM\JoinColumn(name: 'motor', nullable: false, onDelete: 'CASCADE'),
+        /***/
+        Constraints\NotBlank(message: 'car_models.edit.motor.not_blank'),
+    ]
+    private ?Motor $motor = null;
+
+    /**
+     * Nom
+     * @var ?string
+     */
+    #[
+        ORM\Column(type: 'string', length: 50),
+        /***/
+        Constraints\NotBlank(message: 'car_models.edit.name.not_blank'),
+        Constraints\Length(min: 4, max: 100, minMessage: 'car_models.edit.name.min', maxMessage: 'car_models.edit.name.max'),
+    ]
+    private ?string $name;
+
+    /**
+     * Retourne l'identifiant
+     * @return ?int
+     */
+    public function getId() : ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Retourne le moteurs
+     * @return ?Motor
+     */
+    public function getMotor() : ?Motor
+    {
+        return $this->motor;
+    }
+
+    /**
+     * Modifie le moteur
+     * @param ?Motor $motor
+     * @return self
+     */
+    public function setMotor(?Motor $motor) : self
+    {
+        $this->motor = $motor;
+        return $this;
+    }
+
+    /**
+     * Retourne le nom 
+     * @return ?string
+     */
+    public function getName() : ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Modifie le nom
+     * @param string $name
+     * @return self
+     */
+    public function setName(string $name) : self
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
---
+++ src/Repository/CarModelRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Repository pour les modèles de voiture
+ */
+
+namespace App\Repository;
+
+use App\Entity\CarModel;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method CarModel|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CarModel|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CarModel[]    findAll()
+ * @method CarModel[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+final class CarModelRepository extends AbstractRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CarModel::class);
+    }
+
+    // /**
+    //  * @return CarModel[] Returns an array of CarModel objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?CarModel
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
